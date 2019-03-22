76d969889e8aa630968ad6e481bfafb41a94d38f
SA <losemymind@inbox.ru>
2019-03-22 17:12:29 +0300

    наследование в html

--- code/static/particles.js
+++ code/static/particles.js
@@ -1,30 +1,30 @@
 window.onload = function() {
-        Particles.init({
-            selector: '.background',
-            color: '#fcfcfc',
-            connectParticles: 'true',
-            maxParticles: 120,
+    Particles.init({
+        selector: '.background',
+        color: '#fcfcfc',
+        connectParticles: 'true',
+        maxParticles: 120,
 
-            responsive: [
-                {
-                  breakpoint: 768,
-                  options: {
-                    maxParticles: 50,
-                    connectParticles: true
-                  }
-                }, {
-                  breakpoint: 425,
-                  options: {
-                    maxParticles: 30,
-                    connectParticles: true
-                  }
-                }, {
-                  breakpoint: 320,
-                  options: {
-                    maxParticles: 10,
-                    connectParticles: true
-                  }
-                }
-              ]
-        });
-    };
+        responsive: [
+            {
+              breakpoint: 768,
+              options: {
+                maxParticles: 50,
+                connectParticles: true
+              }
+            }, {
+              breakpoint: 425,
+              options: {
+                maxParticles: 30,
+                connectParticles: true
+              }
+            }, {
+              breakpoint: 320,
+              options: {
+                maxParticles: 10,
+                connectParticles: true
+              }
+            }
+          ]
+    });
+};
